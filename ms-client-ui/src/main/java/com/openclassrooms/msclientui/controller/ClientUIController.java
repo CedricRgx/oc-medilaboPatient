cfc2e02f58f3fa78dbe787b55e9e1eb80046754c
CedricRgx <134865690+CedricRgx@users.noreply.github.com>
2024-10-09 17:05:19 +0200

    Enable Spring Security and secure patient list retrieval

--- ms-client-ui/src/main/java/com/openclassrooms/msclientui/controller/ClientUIController.java
+++ ms-client-ui/src/main/java/com/openclassrooms/msclientui/controller/ClientUIController.java
@@ -4,6 +4,10 @@ package com.openclassrooms.msclientui.controller;
 import com.openclassrooms.msclientui.model.Patient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,15 +22,34 @@ public class ClientUIController {
     @Autowired
     private RestTemplate restTemplate;
 
+    private static final String MSPATIENT_API_URL = "http://localhost:8081/patient";
+
     @GetMapping("/patientslist")
     public String getAllPatients(Model model) {
-        log.info("patientsList");
-        List<Patient> patientslist = restTemplate.getForObject("http://localhost:8081/patient/allpatients", List.class);
-        if (patientslist == null) {
-            log.info("The list of patients is null");
-        }
+        log.info("GET request for patients list from ms-patient service");
 
-        model.addAttribute("patientslist", patientslist);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth(System.getenv("MSCLIENTUI_USERNAME"), System.getenv("MSCLIENTUI_PASSWORD"));
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        try {
+            ResponseEntity<List> response = restTemplate.exchange(
+                    MSPATIENT_API_URL + "/allpatients",
+                    HttpMethod.GET,
+                    entity,
+                    List.class
+            );
+
+            List<Patient> patientslist = response.getBody();
+            if (patientslist==null) {
+                log.info("The list of patients is null");
+            }
+            model.addAttribute("patientslist", patientslist);
+        } catch (Exception e) {
+            log.error("Error retrieving the list of patients: ", e);
+            model.addAttribute("error", "Unable to retrieve the list of patients.");
+        }
         return "patientslist";
     }
 }
