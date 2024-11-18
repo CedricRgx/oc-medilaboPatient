5acd4a2a9dbb4e813f3e5589c166da7abd68ddf4
CedricRgx <134865690+CedricRgx@users.noreply.github.com>
2024-11-18 20:36:15 +0100

    Add Dockerfiles for all microservices and configure Docker Compose

--- ms-diabete/src/test/java/com/openclassrooms/msdiabete/model/NoteTest.java
+++ ms-diabete/src/test/java/com/openclassrooms/msdiabete/model/NoteTest.java
@@ -1,10 +1,8 @@
 package com.openclassrooms.msdiabete.model;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
@@ -16,12 +14,6 @@ public class NoteTest {
 
     @InjectMocks
     private Note note;
-
-//    @BeforeEach
-//    public void setup() {
-//        MockitoAnnotations.openMocks(this);
-//        note = new Note();
-//    }
 
     @Test
     public void testGetAndSetId() {
